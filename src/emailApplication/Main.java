975a795e70b33712bbb63901f9e7bf0c49fa4438
AramMkhitaryan93 <artsvaberd21@gmail.com>
2021-02-19 17:54:35 +0400

    my task

---
+++ src/emailApplication/Main.java
@@ -0,0 +1,9 @@
+package emailApplication;
+
+public class Main {
+    public static void main(String[] args) {
+        EmailApp email = new EmailApp("FirstName", "LastName");
+        System.out.println(email.showInfo());
+
+    }
+}
