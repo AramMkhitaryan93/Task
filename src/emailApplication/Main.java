ff9421134288217ccfa15fca8f4a165ed9a84e6a
AramMkhitaryan93 <artsvaberd21@gmail.com>
2021-02-19 18:25:52 +0400

    change emailApp

--- src/emailApplication/Main.java
+++ src/emailApplication/Main.java
@@ -3,6 +3,7 @@ package emailApplication;
 public class Main {
     public static void main(String[] args) {
         EmailApp email = new EmailApp("FirstName", "LastName");
+        EmailApp email1 = new EmailApp("FirstName", "LastName");
         System.out.println(email.showInfo());
 
     }
